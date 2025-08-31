4888f4a7cdca9f8d21e3d94b1fb7d34c51cf4a04
mantotheale <alessiomantonico@gmail.com>
2025-08-31 15:51:10 +0200

    Refactor

---
+++ src/entry_point/mod.rs
@@ -0,0 +1,77 @@
+use std::time::{Duration, Instant};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoopProxy};
+use winit::window::{Window, WindowAttributes, WindowId};
+
+const ONE_SEC_TIME: Duration = Duration::new(1, 0);
+
+pub trait App {
+    fn new(window: Window) -> Self;
+    fn process_input(&mut self, input: WindowEvent);
+    fn update(&mut self);
+    fn one_sec_update(&mut self);
+    fn render(&mut self);
+    fn should_close(&self) -> bool;
+}
+
+pub struct EntryPoint<T: App> {
+    default_window_attributes: WindowAttributes,
+    app: Option<T>,
+    update_span: Duration,
+    next_update_time: Option<Instant>,
+    next_one_sec_time: Option<Instant>,
+    wake_up: EventLoopProxy<()>
+}
+
+impl<T: App> EntryPoint<T> {
+    pub fn new(window_attributes: WindowAttributes, update_span: Duration, wake_up: EventLoopProxy<()>) -> Self {
+        Self {
+            default_window_attributes: window_attributes,
+            app: None,
+            update_span,
+            next_update_time: None,
+            next_one_sec_time: None,
+            wake_up
+        }
+    }
+}
+
+impl<T: App> ApplicationHandler for EntryPoint<T> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop.create_window(self.default_window_attributes.clone()).unwrap();
+
+        self.app = Some(T::new(window));
+
+        let current_time = Instant::now();
+        self.next_update_time = Some(current_time + self.update_span);
+        self.next_one_sec_time = Some(current_time + ONE_SEC_TIME);
+    }
+
+    fn window_event(&mut self, _: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        self.app.as_mut().unwrap().process_input(event);
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        if self.app.as_ref().unwrap().should_close() {
+            event_loop.exit();
+            return;
+        }
+
+        let current_time = Instant::now();
+
+        while current_time >= self.next_update_time.unwrap() {
+            self.app.as_mut().unwrap().update();
+            self.next_update_time = Some(self.next_update_time.unwrap() + self.update_span);
+        }
+
+        self.app.as_mut().unwrap().render();
+
+        while current_time >= self.next_one_sec_time.unwrap() {
+            self.app.as_mut().unwrap().one_sec_update();
+            self.next_one_sec_time = Some(self.next_one_sec_time.unwrap() + ONE_SEC_TIME);
+        }
+
+        self.wake_up.send_event(()).unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
+mod entry_point;
+
 use std::default::Default;
 use std::time::{Duration, Instant};
-use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, EventLoop};
-use winit::window::{Window, WindowAttributes, WindowId};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowAttributes};
+use crate::entry_point::{App, EntryPoint};
 
-const ONE_SEC_TIME: Duration = Duration::new(1, 0);
-const UPDATE_TIME: Duration = Duration::new(0, 16_666_667); // 60 FPS
-
-struct AppState {
+struct Snake {
     window: Window,
     update_count: u32,
-    render_count: u32
+    render_count: u32,
+    should_close: bool
 }
 
-impl AppState {
-    fn update(&mut self) {
-        self.update_count += 1;
+impl App for Snake {
+    fn new(window: Window) -> Self {
+        Self {
+            window,
+            update_count: 0,
+            render_count: 0,
+            should_close: false
+        }
     }
 
-    fn render(&mut self) {
-        self.render_count += 1;
-        self.window.request_redraw();
+    fn process_input(&mut self, input: WindowEvent) {
+        if let WindowEvent::CloseRequested = input {
+            self.should_close = true;
+        }
+
+        println!("{:?}", input);
+    }
+
+    fn update(&mut self) {
+        self.update_count += 1;
     }
 
     fn one_sec_update(&mut self) {
@@ -33,70 +45,29 @@ impl AppState {
         self.render_count = 0;
         println!("Time: {:?}", Instant::now());
     }
-}
 
-#[derive(Default)]
-struct App {
-    default_window_attributes: WindowAttributes,
-    state: Option<AppState>,
-    next_update_time: Option<Instant>,
-    next_one_sec_time: Option<Instant>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = event_loop.create_window(self.default_window_attributes.clone()).unwrap();
-
-        self.state = Some(AppState {
-            window,
-            update_count: 0,
-            render_count: 0,
-        });
-
-        let current_time = Instant::now();
-        self.next_update_time = Some(current_time + UPDATE_TIME);
-        self.next_one_sec_time = Some(current_time + ONE_SEC_TIME);
+    fn render(&mut self) {
+        self.render_count += 1;
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            },
-            WindowEvent::RedrawRequested => {
-                self.state.as_mut().unwrap().render();
-            }
-            _ => ()
-        }
-    }
-
-    fn about_to_wait(&mut self, _: &ActiveEventLoop) {
-        let current_time = Instant::now();
-
-        while current_time >= self.next_update_time.unwrap() {
-            self.state.as_mut().unwrap().update();
-            self.next_update_time = Some(self.next_update_time.unwrap() + UPDATE_TIME);
-        }
-
-        self.state.as_mut().unwrap().render();
-
-        while current_time >= self.next_one_sec_time.unwrap() {
-            self.state.as_mut().unwrap().one_sec_update();
-            self.next_one_sec_time = Some(self.next_one_sec_time.unwrap() + ONE_SEC_TIME);
-        }
+    fn should_close(&self) -> bool {
+        self.should_close
     }
 }
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
 
-    let mut app = App {
-        default_window_attributes: WindowAttributes::default()
-            .with_title("Snake")
-            .with_inner_size(PhysicalSize::new(1280, 720)),
-        ..Default::default()
-    };
+    let window_attributes = WindowAttributes::default()
+        .with_title("Snake")
+        .with_inner_size(PhysicalSize::new(1280, 720));
 
-    event_loop.run_app(&mut app).unwrap();
+    let wake_up = event_loop.create_proxy();
+
+    let mut entry_point = EntryPoint::<Snake>::new(
+        window_attributes, Duration::from_secs_f64(1f64 / 60f64),
+        wake_up
+    );
+
+    event_loop.run_app(&mut entry_point).unwrap();
 }
